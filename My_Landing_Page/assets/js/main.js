d8c5a7cf7570b74dd0f29d16a79ff3d5fb511f0e
alimurni <120017275+alimurni@users.noreply.github.com>
2024-11-15 20:05:42 +0700

    Delete My_Landing_Page directory

--- My_Landing_Page/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector(".btn-primary");
-    button.addEventListener("click", () => {
-        alert("Tombol diklik!");
-    });
-});
-
