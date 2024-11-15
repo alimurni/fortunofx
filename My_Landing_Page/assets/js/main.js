17180b1581f6040d743a146b5720d9719e565431
alimurni <120017275+alimurni@users.noreply.github.com>
2024-11-15 17:56:45 +0700

    First commit: add website structure

---
+++ My_Landing_Page/assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".btn-primary");
+    button.addEventListener("click", () => {
+        alert("Tombol diklik!");
+    });
+});
+
